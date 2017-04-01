e267893fc95a3e5a1f767ccbf9a2184c2be25358
d.wilhelm <daniel.wilhelm@etu.unistra.fr>
2017-04-01 13:33:19 +0200

    Transmission coordonnées agents

--- ConnexionImpl.java
+++ ConnexionImpl.java
@@ -2,9 +2,31 @@ import java.util.ArrayList;
 import java.rmi.server.UnicastRemoteObject ;
 import java.rmi.RemoteException ;
 
-// objet qui va contenir toutes les connexions (objets avec les autres agents
+// objet qui va contenir toutes les connexions (objets avec les autres agents)
+// Une méthode de cette classe est appelée à chaque fois que le controlleur ajoute une machine
 class ConnexionImpl extends UnicastRemoteObject implements Connexion
 {
+    public SerializableList L ;
     
+    public ConnexionImpl()
+    throws RemoteException
+    {
+    }
+    
+    public void initialSet( SerializableList L)
+    throws RemoteException
+    {
+        this.L = L;
+        int i;
+        for( i = 0 ; i< L.size() ; i++)
+            System.out.println("Machine :" + L.get(i).MN + " :" + L.get(i).port);
+    }
+    
+    public void addConnexion(String MachineName, int port)
+    throws RemoteException
+    {
+        L.add(MachineName,port);
+        System.out.println("J'ai ajouté la machine " + MachineName + ":"+ port);
+    }
 	
 }
--- ProducteurMain.java
+++ ProducteurMain.java
@@ -5,7 +5,7 @@ public class ProducteurMain
 {
     static MessageControle M;
     static ProducteurImpl P;
-    
+    static ConnexionImpl C;
     
     public static void main (String [] args)
     {
@@ -21,17 +21,23 @@ public class ProducteurMain
             M = (MessageControle) Naming.lookup("rmi://" + args[0] + ":" + args[1] + "/MessageControleGlobal");
             TripleImpl T =M.getProdInitialInfo();
             System.out.println("Le Producteur reçoit l'id : " + T.x + ", RI : " + T.y + ", RD : " + T.z);
-            P = new ProducteurImpl ( T.x, T.y, T.z);
             
-            // initialise la connexion
+            // initialise le serveur producteur
+            P = new ProducteurImpl ( T.x, T.y, T.z);
             Naming.rebind( "rmi://localhost:"+args[3] + "/Producteur"+ P.id, P);
+            
+            // initialise le serveur connexion pour que le controlleur puisse lui envoyer les nouveaux connectés
+            C = new ConnexionImpl();
+            Naming.rebind( "rmi://localhost:"+args[3] + "/Connexion", C);
+            
+            
             // Maintenant envoie ses "coordonnées" au Coordinateur
             M.addMachine( args[2], Integer.parseInt(args[3]) );
-            
-            je fais un objet dans lequel je mets toutes les connexions avec les Joueurs/Producteurs
-            quand j ajoute un joueur/producteur j envoie un message au Controlleur depuis MessController
-            et le controlleur envoie un message à tous les agents
-            
+            //~ 
+            //~ je fais un objet dans lequel je mets toutes les connexions avec les Joueurs/Producteurs
+            //~ quand j ajoute un joueur/producteur j envoie un message au Controlleur depuis MessController
+            //~ et le controlleur envoie un message à tous les agents
+            //~ 
 			
 		}
         catch (RemoteException re) { System.out.println(re) ; }

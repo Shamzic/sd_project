3eaca9cf8a1e4c3b139591b4c321712692e1a02d
Shamzic2 <shamery2@free.fr>
2017-04-01 16:26:19 +0200

    fin parsing

--- parsing/Test.java
+++ parsing/Test.java
@@ -1,13 +1,16 @@
-import java.io.*;
-
 public class Test{
 
 	public static void main(String[] args) {
 
-		Parser p = new Parser("configuration");
-		int nbjoueurs = p.getNbJoueurs();
-		int nbproducteurs = p.getNbProducteurs();
-		System.out.println(" NB JOUEURS : "+nbjoueurs);
+		Parser p = new Parser();
+		// Parsing du fichier configuration
+		p.lecture_fichier("configuration");
+		String nbjoueurs = p.getNbJoueurs();
+		String nbproducteurs = p.getNbProducteurs();
+		System.out.println("NB JOUEURS : "+nbjoueurs);
 		System.out.println("NB PRODUCTEURS : "+nbproducteurs);
+		// Ici on a les valeurs entières issues du fichier configuration
+		int int_nbjoueurs = Integer.parseInt(nbjoueurs); 
+		int int_nbproducteurs = +Integer.parseInt(nbproducteurs);
 	}
 }

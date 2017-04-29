429ecdadd975a03b27bbb96d409a201b8792a40e
shamzic <shamery@free.fr>
2017-04-29 02:17:41 +0200

    interface de mort

--- Fenetre.java
+++ Fenetre.java
@@ -12,80 +12,218 @@ import java.text.NumberFormat;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Fenetre extends JFrame{
-    int nbJoueurs = 0;
-    int nbProducteurs = 0;
+import javax.swing.JComboBox;
 
-    public void afficherJoueurs(){
-	 System.out.println("Nombre de joueurs : "+nbJoueurs);
+import java.util.ArrayList;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+import javax.swing.JTextArea;
+
+import java.awt.Insets;
+
+public class Fenetre extends JFrame{
+	int nbJoueurs = 0;
+    int nbProducteurs = 1;
+	ArrayList<String> tabJoueurs;
+
+    public Fenetre(){
+		JPanel content = new JPanel();
+		tabJoueurs = new ArrayList<String>();
+
+		this.setTitle("Age of Agents");
+		this.setSize(700, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		NumberFormat format = NumberFormat.getInstance();
+		NumberFormatter formatter = new NumberFormatter(format);
+		formatter.setValueClass(Integer.class);
+		formatter.setMinimum(0);
+		formatter.setMaximum(Integer.MAX_VALUE);
+		formatter.setAllowsInvalid(false);
+		
+		final JFormattedTextField FormatterProducteurs = new JFormattedTextField(formatter);
+		FormatterProducteurs.setValue(1);
+
+		String[] comportements = {"Coopératif", "Individualiste", "Voleur", "Humain"};
+		final JComboBox<String> comboxJoueur= new JComboBox<>(comportements);
+		//comboxJoueur.setSelectedIndex(4);
+
+		final JFormattedTextField FormatterOr = new JFormattedTextField(formatter);
+
+		final JFormattedTextField FormatterBois = new JFormattedTextField(formatter);
+		final JFormattedTextField FormatterArgent = new JFormattedTextField(formatter);
+		JButton BoutonAdd = new JButton("Ajouter joueur");
+		JButton BoutonStart = new JButton("Lancer le jeu");
+		JButton BoutonReset = new JButton("Reset valeurs");
+		
+		
+		
+		final JTextArea textArea = new JTextArea();
+
+		//content.setPreferredSize(new Dimension(300, 120));
+		
+		content.setLayout(new GridBagLayout());
+		
+
+		GridBagConstraints gc = new GridBagConstraints();
+		// marge 
+		gc.weightx = 0.5;
+		gc.weighty = 0.5;
+		// Première ligne
+		gc.gridx=0;
+		gc.gridy=0;
+		gc.anchor = GridBagConstraints.LINE_START;
+		content.add(new JLabel("Ajouter un joueur : "),gc);
+		gc.gridx=1;
+		gc.gridy=0;
+		gc.anchor = GridBagConstraints.CENTER;
+		content.add(comboxJoueur,gc);
+		gc.gridx=2;
+		gc.gridy=0;
+		content.add(BoutonAdd,gc);
+		gc.gridx=3;
+		gc.gridy=0;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		content.add(textArea,gc);
+
+		// Deuxième ligne
+		gc.gridx=0;
+		gc.gridy=1;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		content.add(new JLabel("Nombre producteurs:"),gc);
+		gc.gridx=1;
+		gc.gridy=1;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		content.add(FormatterProducteurs,gc);
+		// Troisième ligne
+		gc.gridx=0;
+		gc.gridy=2;
+		gc.anchor = GridBagConstraints.LINE_START;
+		
+		content.add(new JLabel("Or initial : "),gc);
+		gc.gridx=1;
+		gc.gridy=2;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		content.add(FormatterOr,gc);
+
+
+		// Quatrième ligne
+		gc.gridx=0;
+		gc.gridy=3;
+		gc.anchor = GridBagConstraints.LINE_START;
+		content.add(new JLabel("Bois initial : "),gc);
+		gc.gridx=1;
+		gc.gridy=3;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		content.add(FormatterBois,gc);
+
+		// Cinquième ligne
+		gc.gridx=0;
+		gc.gridy=4;
+		gc.anchor = GridBagConstraints.LINE_START;
+		content.add(new JLabel("Argent initial : "),gc);
+		gc.gridx=1;
+		gc.gridy=4;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		content.add(FormatterArgent,gc);
+
+
+		//On ajoute le bouton au content pane de la JFrame
+/*
+		this.getContentPane().add(comboxJoueur);
+		this.getContentPane().add(BoutonAdd);
+		this.getContentPane().add(new JLabel("Nombre de producteurs"));
+		this.getContentPane().add(FormatterProducteurs);
+		this.getContentPane().add(new JLabel(""));
+
+		this.getContentPane().add(new JLabel("Nombre de ressources d'or"));
+		this.getContentPane().add(FormatterOr);
+		this.getContentPane().add(new JLabel(""));
+		this.getContentPane().add(new JLabel("Nombre de ressources de bois"));
+		this.getContentPane().add(FormatterBois);
+		this.getContentPane().add(new JLabel(""));
+		this.getContentPane().add(new JLabel("Nombre de ressources d'argent"));
+		this.getContentPane().add(FormatterArgent);
+		this.getContentPane().add(new JLabel(""));
+		this.getContentPane().add(BoutonStart);
+		this.getContentPane().add(BoutonReset);
+		this.getContentPane().add(new JLabel(""));
+*/
+
+		BoutonStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				nbJoueurs = getJoueurs();
+				nbProducteurs = (Integer) FormatterProducteurs.getValue();
+				afficherJoueurs();
+			}
+		});
+
+		BoutonReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setJoueurs(0);
+				setProducteurs(1);
+				FormatterProducteurs.setValue(1);
+				tabJoueurs.clear();
+			}
+		});
+
+		BoutonAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String comport = (String) comboxJoueur.getSelectedItem();
+				tabJoueurs.add(comport);
+				setJoueurs(getJoueurs()+1);
+				textArea.setText("");
+				textArea.append("Nombre de joueurs : "+nbJoueurs+"\n");
+				int i=0;
+				while(i<tabJoueurs.size())
+				{
+					textArea.append("Joueur "+i+" "+tabJoueurs.get(i)+"\n");
+					i++;
+				}
+			}
+		});
+
+
+		this.setContentPane(content);
+    	this.setVisible(true);
+  	}
+
+	public void afficherJoueurs(){
+	 	System.out.println("Nombre de joueurs : "+nbJoueurs);
+		int i=0;
+		while(i<tabJoueurs.size())
+		{
+			System.out.println("Joueur "+i+" "+tabJoueurs.get(i));
+			i++;
+		}
+    }
+
+	public void afficherProducteurs(){
+	 	System.out.println("Nombre de joueurs : "+nbJoueurs);
     }
 
     public void setJoueurs(int nbJoueurs){
-	 this.nbJoueurs=nbJoueurs;
+	 	this.nbJoueurs=nbJoueurs;
     }
 
-	public  class TraitementB1 implements ActionListener
-	{
-            public  void actionPerformed(ActionEvent e)
-			{
-				afficherJoueurs();
-			    //System.out.println("Nombre de joueurs : "+Fenetre.nbjoueurs);
-			}
-	}
-    public  class TraitementF1 implements ActionListener
-	{
-	    public void actionPerformed(ActionEvent actionEvent) 
-		{
-		    JFormattedTextField source = (JFormattedTextField) actionEvent.getSource();
-		    Integer value = (Integer)source.getValue();
-		    System.out.println("Value: " + value);
-			setJoueurs((Integer)source.getValue());
-			//System.out.println("nombre de joueurs dans le champ : "+FormatterJoueurs.toString());
-			// MARCHE PAAAS		
-		}
-	}
+    public int getJoueurs(){
+	 	return nbJoueurs;
+    }
 
-  public Fenetre(){
-	this.setTitle("Age of Agents");
-    this.setSize(600, 100);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-	NumberFormat format = NumberFormat.getInstance();
-    NumberFormatter formatter = new NumberFormatter(format);
-    formatter.setValueClass(Integer.class);
-    formatter.setMinimum(0);
-    formatter.setMaximum(Integer.MAX_VALUE);
-    formatter.setAllowsInvalid(false);
-    JFormattedTextField FormatterJoueurs =new JFormattedTextField(formatter);
-    JFormattedTextField FormatterProducteurs =new JFormattedTextField(formatter);
+	public void setProducteurs(int nbProducteurs){
+	 	this.nbProducteurs=nbProducteurs;
+    }
 
-
-    
-    JButton BoutonJoueurs = new JButton("OK");
-    JButton BoutonProducteurs = new JButton("OK");
-    JButton BoutonStart = new JButton("Lancer le jeu");
-
-    this.setLayout(new GridLayout(3, 3));
-
-    //On ajoute le bouton au content pane de la JFrame
-    this.getContentPane().add(new JLabel("Nombre de joueurs"));
-    this.getContentPane().add(FormatterJoueurs);
-    this.getContentPane().add(BoutonJoueurs);
-
-    this.getContentPane().add(new JLabel("Nombre de producteurs"));
-    this.getContentPane().add(FormatterProducteurs);
-    this.getContentPane().add(BoutonProducteurs);
-
-    this.getContentPane().add(BoutonStart);
-
-  FormatterJoueurs.addActionListener(new TraitementF1());
-  //FormatterProducteurs.addActionListener(actionListenerForm);
-  BoutonJoueurs.addActionListener(new TraitementB1());
-
-    this.setVisible(true);
-
-  }
-
-	
-
+	public int getProducteurs(){
+	 	return nbProducteurs;
+    }
 }

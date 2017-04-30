9c1de1a9258c802892e5e6d24fb84a3b08184782
shamzic <shamery@free.fr>
2017-04-30 14:37:33 +0200

    interface avance

--- Controller.java
+++ Controller.java
@@ -11,9 +11,9 @@ public class Controller
 		{
             // Fait une liste de ressource qu'il faut pour gagner
             SerializableList<Ressource> L = new SerializableList<Ressource>();
-            L.add(new Ressource(9,0));
-            L.add(new Ressource(50,1));
-            L.add(new Ressource(18,2));
+            L.add(new Ressource(9,0)); // Argent
+            L.add(new Ressource(50,1)); // Or
+            L.add(new Ressource(18,2)); // bois
 
 	    // Commence par faire l'objet grâce auquel le Controlleur communique avec les agents
             MessageControleImpl MC = new MessageControleImpl(5,3, Integer.parseInt(args[1]),Integer.parseInt(args[0]),"localhost",5000,0,L);
--- Fenetre.java
+++ Fenetre.java
@@ -26,10 +26,21 @@ import javax.swing.JTextArea;
 
 import java.awt.Insets;
 
+import java.util.ArrayList;
+import java.rmi.server.UnicastRemoteObject ;
+import java.rmi.RemoteException ;
+import java.rmi.* ; 
+import java.net.MalformedURLException ; 
+
 public class Fenetre extends JFrame{
 	int nbJoueurs = 0;
     int nbProducteurs = 1;
 	ArrayList<String> tabJoueurs;
+	boolean start =false;
+	int orWin=1;
+	int argentWin=1;
+	int boisWin=1;
+	int Tps_max=30;
 
     public Fenetre(){
 		JPanel content = new JPanel();
@@ -51,12 +62,25 @@ public class Fenetre extends JFrame{
 
 		String[] comportements = {"Coopératif", "Individualiste", "Voleur", "Humain"};
 		final JComboBox<String> comboxJoueur= new JComboBox<>(comportements);
-		//comboxJoueur.setSelectedIndex(4);
 
-		final JFormattedTextField FormatterOr = new JFormattedTextField(formatter);
-
-		final JFormattedTextField FormatterBois = new JFormattedTextField(formatter);
-		final JFormattedTextField FormatterArgent = new JFormattedTextField(formatter);
+		NumberFormatter formatterRessources = new NumberFormatter(format);
+		formatterRessources.setMinimum(5);
+		formatterRessources.setMaximum(Integer.MAX_VALUE);
+		formatterRessources.setAllowsInvalid(false);
+		
+		final JFormattedTextField FormatterOr = new JFormattedTextField(formatterRessources);
+		FormatterOr.setValue(5);
+		final JFormattedTextField FormatterBois = new JFormattedTextField(formatterRessources);
+		FormatterBois.setValue(5);
+		final JFormattedTextField FormatterArgent = new JFormattedTextField(formatterRessources);
+		FormatterArgent.setValue(5);
+		NumberFormatter formatterTemps = new NumberFormatter(format);
+		formatterTemps.setMinimum(20);
+		formatterTemps.setMaximum(Integer.MAX_VALUE);
+		formatterTemps.setAllowsInvalid(false);
+		
+		final JFormattedTextField FormatterTempsMax = new JFormattedTextField(formatterTemps);
+		FormatterTempsMax.setValue(20);
 		JButton BoutonAdd = new JButton("Ajouter joueur");
 		JButton BoutonStart = new JButton("Lancer le jeu");
 		JButton BoutonReset = new JButton("Reset valeurs");
@@ -75,6 +99,7 @@ public class Fenetre extends JFrame{
 		gc.weightx = 0.5;
 		gc.weighty = 0.5;
 		// Première ligne
+		
 		gc.gridx=0;
 		gc.gridy=0;
 		gc.anchor = GridBagConstraints.LINE_START;
@@ -106,7 +131,7 @@ public class Fenetre extends JFrame{
 		gc.gridy=2;
 		gc.anchor = GridBagConstraints.LINE_START;
 		
-		content.add(new JLabel("Or initial : "),gc);
+		content.add(new JLabel("Or pour la victoire : "),gc);
 		gc.gridx=1;
 		gc.gridy=2;
 		gc.gridwidth = GridBagConstraints.REMAINDER;
@@ -118,7 +143,7 @@ public class Fenetre extends JFrame{
 		gc.gridx=0;
 		gc.gridy=3;
 		gc.anchor = GridBagConstraints.LINE_START;
-		content.add(new JLabel("Bois initial : "),gc);
+		content.add(new JLabel("Bois pour la victoire : "),gc);
 		gc.gridx=1;
 		gc.gridy=3;
 		gc.gridwidth = GridBagConstraints.REMAINDER;
@@ -129,42 +154,64 @@ public class Fenetre extends JFrame{
 		gc.gridx=0;
 		gc.gridy=4;
 		gc.anchor = GridBagConstraints.LINE_START;
-		content.add(new JLabel("Argent initial : "),gc);
+		content.add(new JLabel("Argent pour la victoire : "),gc);
 		gc.gridx=1;
 		gc.gridy=4;
 		gc.gridwidth = GridBagConstraints.REMAINDER;
 		gc.fill = GridBagConstraints.HORIZONTAL;
 		content.add(FormatterArgent,gc);
+		// Sixième ligne
+		gc.gridx=0;
+		gc.gridy=5;
+		gc.anchor = GridBagConstraints.LINE_START;
+		content.add(new JLabel("Temps max : "),gc);
+		gc.gridx=1;
+		gc.gridy=5;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		content.add(FormatterTempsMax,gc);
+		
+		// Septième ligne
+		gc.insets = new Insets(10,10,10,10);
+		gc.gridwidth=2; 
+		gc.gridx=0;
+		gc.gridy=6;
+		gc.anchor = GridBagConstraints.LINE_START;
+		content.add(BoutonStart,gc);
+		gc.gridwidth=1; 
+		gc.gridx=2;
+		gc.gridy=6;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		content.add(BoutonReset,gc);
 
-
-		//On ajoute le bouton au content pane de la JFrame
-/*
-		this.getContentPane().add(comboxJoueur);
-		this.getContentPane().add(BoutonAdd);
-		this.getContentPane().add(new JLabel("Nombre de producteurs"));
-		this.getContentPane().add(FormatterProducteurs);
-		this.getContentPane().add(new JLabel(""));
-
-		this.getContentPane().add(new JLabel("Nombre de ressources d'or"));
-		this.getContentPane().add(FormatterOr);
-		this.getContentPane().add(new JLabel(""));
-		this.getContentPane().add(new JLabel("Nombre de ressources de bois"));
-		this.getContentPane().add(FormatterBois);
-		this.getContentPane().add(new JLabel(""));
-		this.getContentPane().add(new JLabel("Nombre de ressources d'argent"));
-		this.getContentPane().add(FormatterArgent);
-		this.getContentPane().add(new JLabel(""));
-		this.getContentPane().add(BoutonStart);
-		this.getContentPane().add(BoutonReset);
-		this.getContentPane().add(new JLabel(""));
-*/
 
 		BoutonStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				nbJoueurs = getJoueurs();
 				nbProducteurs = (Integer) FormatterProducteurs.getValue();
 				afficherJoueurs();
-			}
+
+				/* Lancement controlleur */
+				
+					try
+					{
+						// Fait une liste de ressource qu'il faut pour gagner
+						
+						SerializableList<Ressource> L = new SerializableList<Ressource>();
+						L.add(new Ressource(argentWin/10,0)); // Argent
+						L.add(new Ressource(orWin/10,1)); // Or 
+						L.add(new Ressource(boisWin/10,2)); // Bois
+
+					// Commence par faire l'objet grâce auquel le Controlleur communique avec les agents
+						MessageControleImpl MC = new MessageControleImpl(5,3,nbProducteurs,nbJoueurs,"localhost",5000,0,L);
+						Naming.rebind( "rmi://localhost:"+5000 +"/MessageControleGlobal", MC); 
+					}
+					catch (RemoteException re) { System.out.println(re) ; }
+					catch (MalformedURLException excep) { System.out.println(excep) ; }
+				}
+
+			
+			
 		});
 
 		BoutonReset.addActionListener(new ActionListener() {
@@ -173,6 +220,7 @@ public class Fenetre extends JFrame{
 				setProducteurs(1);
 				FormatterProducteurs.setValue(1);
 				tabJoueurs.clear();
+				textArea.setText("");
 			}
 		});
 
@@ -211,19 +259,50 @@ public class Fenetre extends JFrame{
 	 	System.out.println("Nombre de joueurs : "+nbJoueurs);
     }
 
+
+	// Setteurs
+	
     public void setJoueurs(int nbJoueurs){
 	 	this.nbJoueurs=nbJoueurs;
     }
 
-    public int getJoueurs(){
-	 	return nbJoueurs;
-    }
 
 	public void setProducteurs(int nbProducteurs){
 	 	this.nbProducteurs=nbProducteurs;
     }
+    
+	public void setOrWin(int orWin){
+	 	this.orWin=orWin;
+    }
+    
+    public void setArgentWin(int argentWin){
+	 	this.argentWin=argentWin;
+    }
+    public void setBoisWin(int boisWin){
+	 	this.boisWin=boisWin;
+    }    
+    
+    // Getteurs
+    
+    public int getJoueurs(){
+	 	return this.nbJoueurs;
+    }
 
 	public int getProducteurs(){
-	 	return nbProducteurs;
+	 	return this.nbProducteurs;
     }
+
+	public int getOrWin(){
+	 	return this.orWin;
+    }
+    
+    public int getArgentWin(){
+	 	return this.argentWin;
+    }
+    public int getBoisWin(){
+	 	return this.boisWin;
+    } 
+    
 }
+
+

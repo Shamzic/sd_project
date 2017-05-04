3e9cca434f89b0fe8b6b80ea12fb0c512ffbc0f3
d.wilhelm <daniel.wilhelm@etu.unistra.fr>
2017-05-04 23:58:19 +0200

    ..

--- Controller.java
+++ Controller.java
@@ -16,7 +16,7 @@ public class Controller
             L.add(new Ressource(100,2));
 
 	    // Commence par faire l'objet grâce auquel le Controlleur communique avec les agents
-            MessageControleImpl MC = new MessageControleImpl(5,3, Integer.parseInt(args[1]),Integer.parseInt(args[0]),"localhost",5000,0,L, 0);
+            MessageControleImpl MC = new MessageControleImpl(5,3, Integer.parseInt(args[1]),Integer.parseInt(args[0]),"localhost",5000,0,L, 1,5);
             Naming.rebind( "rmi://localhost:"+5000 +"/MessageControleGlobal", MC); 
 		}
         catch (RemoteException re) { System.out.println(re) ; }
